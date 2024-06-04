6b5b0f32d3ee943a67a4d7db434b53d75f4139dc
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-06-03 18:46:29 -0600

    controller

--- src/business/Controller.java
+++ src/business/Controller.java
@@ -1,5 +1,18 @@
 package business;
 
+import presentation.UI;
+
 public class Controller {
+	
+	private UI ui;
+	
+	public Controller(UI ui) {
+		this.ui=ui;
+	}
+
+	public void getControl() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
