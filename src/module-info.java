79c5a5e43605f17c89df01c92cc70911aff503d9
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-06-06 00:28:28 -0600

    se borro guiWelcome

--- src/module-info.java
+++ src/module-info.java
@@ -4,9 +4,13 @@ module Test{
 	requires java.desktop;
 	requires javafx.fxml;
     requires java.xml;
+	requires javafx.graphics;
 
 	opens business to javafx.graphics, javafx.fmxl;
 	
 	exports presentation;
+	exports application;
     exports domain;
+    
+    opens application to javafx.fxml;
 }
