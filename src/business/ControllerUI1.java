a8a164bb3ee7f914c6eba69b68b8004326011152
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-06-10 10:22:52 -0600

    ControllerUI1

---
+++ src/business/ControllerUI1.java
@@ -0,0 +1,24 @@
+package business;
+
+import presentation.UI;
+import presentation.UI1;
+
+public class ControllerUI1 {
+
+	private UI1 ui1;
+	private UI ui;
+	
+	public ControllerUI1(UI1 ui1) {
+		this.ui1=ui1;
+	}
+//-----------------------------------------------------
+	public void getControl() {
+		
+		this.ui1.getBPlay().setOnAction(e->{
+					
+				ui= new UI();
+				new Controller(ui);
+				
+				});
+			}
+}
